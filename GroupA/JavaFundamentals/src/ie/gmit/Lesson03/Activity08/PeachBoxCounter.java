95ffb9ff6df4450eb3c1680f9ecb03e64c7f3022
mmurray <michael.murray@gmit.ie>
2021-02-01 16:02:01 +0000

    Mon 1/2/2021 16:00

--- GroupA/JavaFundamentals/src/ie/gmit/Lesson03/Activity08/PeachBoxCounter.java
+++ GroupA/JavaFundamentals/src/ie/gmit/Lesson03/Activity08/PeachBoxCounter.java
@@ -3,13 +3,16 @@ package ie.gmit.Lesson03.Activity08;
 import java.util.Scanner;
 
 public class PeachBoxCounter {
-  static Scanner sc = new Scanner(System.in);
+
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number of peaches picked: ");
     int numberOfPeaches = sc.nextInt();
+
     for (int numShipped = 0; numShipped < numberOfPeaches; numShipped += 20) {
       System.out.printf("shipped %d peaches so far\n", numShipped);
     }
+
   }
 }
