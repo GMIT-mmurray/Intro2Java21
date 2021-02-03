bc45fa646594ff322904b2cb6b7a7b4a0c248154
mmurray <michael.murray@gmit.ie>
2021-02-03 09:58:36 +0000

    Wed 3/2/2021 10:00

--- GroupA/JavaFundamentals/src/ie/gmit/Lesson04/Activity12/Animal.java
+++ GroupA/JavaFundamentals/src/ie/gmit/Lesson04/Activity12/Animal.java
@@ -1,11 +1,12 @@
 package ie.gmit.Lesson04.Activity12;
 
 public class Animal {
-  int legs;
-  int ears;
-  int eyes;
-  String family;
-  String name;
+
+  private int legs;
+  private int ears;
+  private int eyes;
+  private String family;
+  private String name;
 
   public Animal() {
     this(4, 2, 2);
@@ -15,7 +16,6 @@ public class Animal {
     this.legs = legs;
     this.ears = ears;
     this.eyes = ears;
-
   }
 
   public String getFamily() {
@@ -34,23 +34,8 @@ public class Animal {
     this.name = name;
   }
 
-  public static void main(String[] args) {
-    Animal cow = new Animal();
-    Animal goat = new Animal();
-    Animal duck = new Animal(2, 2, 2);
+  public int getLegs() { return legs; }
 
-    cow.setName("Cow");
-    cow.setFamily("Bovidae");
-
-    goat.setName("Goat");
-    goat.setFamily("Bovidae");
-    duck.setName("Duck");
-    duck.setFamily("Anatidae");
-
-    System.out.println(cow.getName());
-    System.out.println(goat.getName());
-    System.out.println(duck.getFamily());
-
+  public void setLegs(int legs) { this.legs = legs;
   }
-
 }
