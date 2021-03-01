5dd21483202b59d0c932b00ce83fc14af6829d7f
mmurray <michael.murray@gmit.ie>
2021-03-01 20:18:46 +0000

    Mon 1/3/2021

---
+++ GroupB/String/String/src/ie/gmit/StringTokenizerTest.java
@@ -0,0 +1,23 @@
+package ie.gmit;
+
+import java.util.StringTokenizer;
+/**
+ * Reverse the words in a String using StringTokenizer
+ */
+class StringTokenizerTest {
+    public static void main(String[] args) {
+        String str = "Monday Tuesday Wednesday Thursday Friday Saturday Sunday";
+        String strReverse;
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(str);
+
+        while (st.hasMoreTokens()) {
+            sb.insert(0, st.nextToken());
+            if (st.hasMoreTokens()) {
+                sb.insert(0, " ");
+            }
+        }
+        strReverse = sb.toString();
+        System.out.println(strReverse);
+    }
+}
